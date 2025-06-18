f25d3b1c730b761bc9b892a2328393f6b0b103d3
emmy <52672675+emmaccode@users.noreply.github.com>
2025-06-18 11:10:16 -0600

    more progression update

--- src/AlgebraStreamFrames.jl
+++ src/AlgebraStreamFrames.jl
@@ -1,7 +1,6 @@
 module AlgebraStreams
 using AlgebraFrames
-
-SUPPORTED_FILES = [:ff, :csv, :json]
+using AlgebraFrames: Transform
 
 function infer_type(fp::String)
     if fp == ""
@@ -23,13 +22,17 @@ function infer_type(fp::String)
 end
 
 mutable struct StreamFrame{T <: Any} <: AlgebraFrames.AbstractAlgebraFrame
-	paths::Dict{String, String}  # Mapping column names to file paths
-	n::Int64                     # Number of rows (determined from files)
-	names::Vector{String}         # Column names
-	types::Vector{Type}           # Data types for each column
-	algebra::Vector{Algebra{<:Any, 1}}
-    function StreamFrame{T}(n::Int64, info::Dict{String, String}, names::Vector{String}, types::Vector{Type}, algebra::Vector{Algebra{<:Any, 1}})
-        new{T}(info, n, names, types, algebra)
+	paths::Dict{String, String} 
+	length::Int64
+    T::Vector{Type}           
+	names::Vector{String}
+    gen::Vector{Function}     
+	         
+	transformations::Vector{Transform}
+    function StreamFrame{T}(n::Int64, info::Dict{String, String}, names::Vector{String}, 
+        gen::Vector{Function}, types::Vector{Type}, transforms::Vector{AlgebraFrames.Transform} = 
+        Vector{Transform}())
+        new{T}(info, n, names, types, transforms)
     end
 end
 
@@ -41,9 +44,9 @@ function StreamFrame(named_paths::Pair{String, String} ...)
         touch(first_file)
     end
     n = countlines(first_file)  # Assume all files have same row count
-    @info n
     if n == 0
-        return(new{Symbol(:feature_file)}(paths, n, names, Vector{Symbol}(), Vector{Algebra{<:Any, 1}}()))
+        return(StreamFrame{Symbol(:feature_file)}(paths,
+            n, names, Vector{Symbol}(), Vector{Algebra{<:Any, 1}}()))
     end
     types = [begin
         if ~(isfile(fp))
@@ -52,38 +55,44 @@ function StreamFrame(named_paths::Pair{String, String} ...)
         @info fp
         infer_type(readlines(fp)[1])
     end for fp in values(paths)]  # Infer types
-    @info types
-    alg = [begin
-            T = types[enum]
-            if T == String
-                algebra(types[enum], n) do e::Int64
-                    readlines(paths[names[enum]])[e]
-                end
-            else
-                algebra(types[enum], n) do e::Int64
-                    parse(T, readlines(paths[names[enum]])[e])
-                end
-            end
-        end for enum in 1:length(names)]
-    @info alg
-    StreamFrame{Symbol(:feature_file)}(n, paths, names, types, alg)  # Default type
+    gen::Vector{Function} = [begin 
+        if T == String
+            e::Int64 -> readlines(paths[names[enum]])[e]
+        else
+            e::Int64 -> parse(T, readlines(paths[names[enum]])[e])
+        end
+    end for enum in 1:length(names)]
+    StreamFrame{Symbol(:feature_file)}(n, paths, names, gen, types)  # Default type
 end
 
-function StreamFrame(path::String, T::Symbol = :ff)
-    if T == :csv
-        data = read_csv(path)
-    elseif T == :json
-        data = read_json(path)
-    else
-        error("Unsupported file type: $T")
+function StreamFrame(path::String)
+    if ~(contains(path, "."))
+        # TODO
     end
-
+    f_typespl = split(path, ".")[2:end]
+    df = DataFileType{Symbol(f_typespl)}
+    algebraic_read(df, path)
     names = keys(data)
     n = length(data[names[1]])  # Assume all columns have same length
     types = [eltype(col) for col in values(data)]
 
     paths = Dict(name => path for name in names)  # Single-file storage
-    StreamFrame{T}(n, paths names, types, [])
+    StreamFrame{T}(n, paths names, types)
 end
+
+struct DataFileType{T} end
+
+function algebraic_read(T::Type{DataFileType{<:Any}}, path::String)
+
+end
+
+function algebraic_read(T::Type{DataFileType{:json}}, path::String)
+
+end
+
+function algebraic_read(T::Type{DataFileType{:csv}}, path::String)
+    StreamFrame{:csv}()
+end
+
 
 end # module AlgebraStreamFrames
